6b5a2842f7dfa642635a63f86e036a97c4b25905
Michał Krakiewicz <michalkrakiewicz@hotmail.com>
2017-08-12 17:16:01 +0200

    Amendments

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,7 +35,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'cors',
+//            'cors',
             'throttle:60,1',
             'bindings',
         ],
@@ -55,6 +55,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'cors' => \App\Http\Middleware\Cors::class
+//        'cors' => \App\Http\Middleware\Cors::class
     ];
 }
